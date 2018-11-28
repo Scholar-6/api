f7f68cfc1a472ad9dd2f77fd6d34ffd6fc6a74cf
aromakh <andriyromakh@gmail.com>
2018-11-28 10:49:11 +0200

    Loading swagger file locally with express

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 var fs = require('fs'),
     path = require('path'),
+    express = require('express'),
     http = require('http'),
     https = require('https');
 
@@ -11,6 +12,8 @@ var jsyaml = require('js-yaml');
 var serverPort = 8080;
 var swaggerDoc;
 
+app.use(express.static(__dirname + '/api'));
+
 // swaggerRouter configuration
 var options = {
   swaggerUi: path.join(__dirname, '/swagger.json'),
@@ -19,7 +22,31 @@ var options = {
 };
 
 // The Swagger document (require it, build it programmatically, fetch it from a URL, ...)
-// var spec = fs.readFileSync(path.join(__dirname,'api/swagger.yaml'), 'utf8');
+var documentData = fs.readFileSync(path.join(__dirname,'api/swagger.yaml'), 'utf8');
+var swaggerDoc = jsyaml.safeLoad(documentData);
+// Initialize the Swagger middleware
+swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
+  // Interpret Swagger resources and attach metadata to request - must be first in swagger-tools middleware chain
+  app.use(middleware.swaggerMetadata());
+
+  // Validate Swagger requests
+  app.use(middleware.swaggerValidator());
+
+  // Route validated requests to appropriate controller
+  app.use(middleware.swaggerRouter(options));
+
+  // Serve the Swagger documents and Swagger UI
+  app.use(middleware.swaggerUi());
+
+  // Start the server
+  http.createServer(app).listen(serverPort, function () {
+    console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
+    console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
+  });
+
+});
+
+/* Get yaml from main branch
 var spec = https.get("https://raw.githubusercontent.com/Scholar-6/api/master/api/swagger.yaml", function (res) {
     res.setEncoding('utf8');
 
@@ -56,5 +83,4 @@ swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
 
 });
 });
-
-
+*/
