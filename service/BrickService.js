12e9050d59332c59f5c3972e5df6d2a271e504ab
aromakh <andriyromakh@gmail.com>
2020-10-12 17:28:24 +0300

    get public brick by id without signup

--- service/BrickService.js
+++ service/BrickService.js
@@ -281,6 +281,62 @@ exports.brickIdGET = function(id) {
 
 
 /**
+ * Get specific public brick by id
+ *
+ * id Integer 
+ * returns Brick
+ **/
+exports.brickPublicIdGET = function(id) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = {
+  "alternativeSubject" : "alternativeSubject",
+  "subTopic" : "subTopic",
+  "brief" : "brief",
+  "synthesis" : "synthesis",
+  "openQuestion" : "openQuestion",
+  "subject" : {
+    "color" : "color",
+    "name" : "name",
+    "checked" : true,
+    "id" : 5,
+    "bricksCount" : 5,
+    "publishedBricksCount" : 2
+  },
+  "created" : "2000-01-23T04:56:07.000+00:00",
+  "questions" : [ {
+    "contentBlocks" : "contentBlocks",
+    "id" : 7,
+    "questionType" : "None",
+    "order" : 9
+  }, {
+    "contentBlocks" : "contentBlocks",
+    "id" : 7,
+    "questionType" : "None",
+    "order" : 9
+  } ],
+  "revisionLog" : "revisionLog",
+  "title" : "title",
+  "subjectId" : 1,
+  "attemptsCount" : 6,
+  "brickLength" : "0",
+  "alternativeTopics" : "alternativeTopics",
+  "topic" : "topic",
+  "prep" : "prep",
+  "id" : 0,
+  "locked" : true,
+  "updated" : "2000-01-23T04:56:07.000+00:00"
+};
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
  * Set brick to publish status
  * 
  *
